21df29e292fe2529ab4e787644f08a2323d4be67
hfortin <h.fortin@alkante.com>
2018-12-27 10:33:10 +0100

    Initialisation du projet

---
+++ src/main/java/fr/hadf/timwi/TimwiApplication.java
@@ -0,0 +1,14 @@
+package fr.hadf.timwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimwiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimwiApplication.class, args);
+	}
+
+}
+
