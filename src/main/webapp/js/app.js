d87978cf96d0d800888f8779c962b7b969563e61
hfortin <h.fortin@alkante.com>
2018-12-27 18:28:41 +0100

    Frontend

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,44 @@
+Vue.component('modal', {
+	template: '#modal-template'
+})
+
+window.onload = function () {
+	new Vue({
+		el: '#heroes',
+		data : {
+			showModal: false,
+			heroesList : [
+				{
+					id: 0,
+					name : 'Captain America',
+					thumbnail: '/img/0.jpg',
+					description: 'Captain America est le super héro de l\'Amérique'
+				},
+				{
+					id: 1,
+					name : 'Thor',
+					thumbnail: '/img/1.jpg',
+					description: 'Thor est le dieu de la foudre'
+				},
+				{
+					id: 2,
+					name : 'Iron Man',
+					thumbnail: '/img/2.jpg',
+					description: 'Iron Man est le milliardaire philanthrope'
+				}
+			]
+		},
+		methods: {
+			promptAlbum: function() {
+				var id = event.srcElement.getAttribute('data-id');
+				this.showModal = true;
+				this.heroId = id;
+			},
+			addHero: function(event) {
+				this.$http.put('/favorite/add/'+this.album+'/'+this.heroId).then(function() {
+					
+				});
+			}
+		}
+	});
+}
